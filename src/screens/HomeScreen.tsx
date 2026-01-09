f87ffbd462489aa9e77343eb1a0c1e0963c765ac
Eli Gauger <egaugs@Mac.lan>
2026-01-08 18:47:23 -0600

    good enough for a while 6

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -366,7 +366,7 @@ const styles = StyleSheet.create({
         width: '100%',
         justifyContent: 'center',
         alignItems: 'center',
-        minHeight: 500, 
+        minHeight: 300, 
         marginBottom: 150, 
     },
     projectName: {
